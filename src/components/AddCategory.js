52a5ad6b3ca1fa261302da3a2eef9bc5d4db39b8
Wladimir <wld.rocha@gmail.com>
2021-07-25 13:23:19 -0400

    change var name

--- src/components/AddCategory.js
+++ src/components/AddCategory.js
@@ -3,23 +3,24 @@ import PropTypes from 'prop-types'
 
 export const AddCategory = ({ setCategories }) => {
 
-    const [inputaValue, setInputaValue] = useState('')
+    const [inputValue, setInputValue] = useState('')
 
     const handleInputChange = (e) => {
-        setInputaValue(e.target.value)
+        setInputValue(e.target.value)
     }
 
     const handleSubmit = (e) => {
         e.preventDefault()
-        if(inputaValue.trim().length >2){
-            setCategories(cats => [ inputaValue, ...cats ])
-            setInputaValue('')
+
+        if(inputValue.trim().length >2){
+            setCategories(cats => [ inputValue, ...cats ])
+            setInputValue('')
         }
     }
 
     return (
         <form onSubmit={ handleSubmit }>
-            <input type="text" onChange={ handleInputChange } value={ inputaValue } />
+            <input type="text" onChange={ handleInputChange } value={ inputValue } />
         </form>
     )
 }
